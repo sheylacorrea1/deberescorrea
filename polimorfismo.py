c7b877d2a322d4a074f5dffb570ac81ab6e8f8c1
Sheyla <sd.correap@uea.edu.ec>
2023-12-09 18:40:51 -0500

    polimorfismo

--- polimorfismo.py
+++ polimorfismo.py
@@ -4,10 +4,21 @@ class Animal:
 class Cat(Animal):
 	def makeNoise(self):
 		print("Meoooowwwww")
+<<<<<<< HEAD
 class Dog(Animal):
 	def makeNoise(self):
 		print("Woooooof")
 a = Cat()
 a.makeNoise() #Prints Meeeowwwwww
+=======
+
+class Dog(Animal):
+	def makeNoise(self):
+		print("Woooooof")
+
+a = Cat()
+a.makeNoise() #Prints Meeeowwwwww
+
+>>>>>>> b41d6d5 (polimorfismo)
 a = Dog()
 a.makeNoise() #Prints Woooooof

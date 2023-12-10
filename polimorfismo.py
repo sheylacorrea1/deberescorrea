726f0d4b21205ac691546c016eee53dd002ab1c3
Sheyla <sd.correap@uea.edu.ec>
2023-12-09 19:22:41 -0500

    polimor

--- polimorfismo.py
+++ polimorfismo.py
@@ -1,24 +1,17 @@
 class Animal:
 	def makeNoise(self):
 		raise NotImplementedError
+
 class Cat(Animal):
 	def makeNoise(self):
 		print("Meoooowwwww")
-<<<<<<< HEAD
-class Dog(Animal):
-	def makeNoise(self):
-		print("Woooooof")
-a = Cat()
-a.makeNoise() #Prints Meeeowwwwww
-=======
 
 class Dog(Animal):
 	def makeNoise(self):
 		print("Woooooof")
 
-a = Cat()
+a = Cat();
 a.makeNoise() #Prints Meeeowwwwww
 
->>>>>>> b41d6d5 (polimorfismo)
-a = Dog()
+a = Dog();
 a.makeNoise() #Prints Woooooof

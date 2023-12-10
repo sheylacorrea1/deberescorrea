74ea4dad3b3781f9092420f65ac0721286d1d788
Sheyla <sd.correap@uea.edu.ec>
2023-12-09 19:12:48 -0500

    polimor

---
+++ polimor.py
@@ -0,0 +1,13 @@
+class Animal:
+	def makeNoise(self):
+		raise NotImplementedError
+class Cat(Animal):
+	def makeNoise(self):
+		print("Meoooowwwww")
+class Dog(Animal):
+	def makeNoise(self):
+		print("Woooooof")
+a = Cat()
+a.makeNoise() #Prints Meeeowwwwww
+a = Dog()
+a.makeNoise() #Prints Woooooof
